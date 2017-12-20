6d13eedecdc20dcb5432096448897bef79b2ed46
Jacob Lewallen <jlewallen@gmail.com>
2017-12-20 09:15:12 -0800

    Add flash memory to firmware test.

--- firmware/test/main.cpp
+++ firmware/test/main.cpp
@@ -8,39 +8,14 @@
 #include <Adafruit_MPL3115A2.h>
 #include <Adafruit_TSL2591.h>
 #include <Adafruit_SHT31.h>
+#include <SerialFlash.h>
+
+const uint8_t PIN_FLASH_CS = 5;
 
 Adafruit_TSL2591 tsl(2591);
 
 TwoWire myWire1(&sercom1, 11, 13);
 TwoWire myWire2(&sercom2, 4, 3);
-
-/*
-class Adafruit_SHT31 {
-private:
-    uint8_t address;
-
-private:
-    uint8_t read8(uint8_t a) {
-        Wire.beginTransmission(address);
-        Wire.write(a);
-        Wire.endTransmission(false);
-
-        Wire.requestFrom((uint8_t)address, (uint8_t)1);
-        return Wire.read();
-    }
-
-public:
-    Adafruit_SHT31(uint8_t address) {
-        this->address = address;
-    }
-
-    bool begin() {
-        uint8_t value = read8(0x12);
-        Serial.println(value);
-        return value == 0x50;
-    }
-};
-*/
 
 class Adafruit_TSL25911FN {
 private:
@@ -72,6 +47,66 @@ public:
 };
 
 class Check {
+private:
+    const char * id2chip(const unsigned char *id) {
+        if (id[0] == 0xEF) {
+            // Winbond
+            if (id[1] == 0x40) {
+                if (id[2] == 0x14) return "W25Q80BV";
+                if (id[2] == 0x15) return "W25Q16DV";
+                if (id[2] == 0x17) return "W25Q64FV";
+                if (id[2] == 0x18) return "W25Q128FV";
+                if (id[2] == 0x19) return "W25Q256FV";
+            }
+        }
+        if (id[0] == 0x01) {
+            // Spansion
+            if (id[1] == 0x02) {
+                if (id[2] == 0x16) return "S25FL064A";
+                if (id[2] == 0x19) return "S25FL256S";
+                if (id[2] == 0x20) return "S25FL512S";
+            }
+            if (id[1] == 0x20) {
+                if (id[2] == 0x18) return "S25FL127S";
+            }
+            if (id[1] == 0x40) {
+                if (id[2] == 0x15) return "S25FL116K";
+            }
+        }
+        if (id[0] == 0xC2) {
+            // Macronix
+            if (id[1] == 0x20) {
+                if (id[2] == 0x18) return "MX25L12805D";
+            }
+        }
+        if (id[0] == 0x20) {
+            // Micron
+            if (id[1] == 0xBA) {
+                if (id[2] == 0x20) return "N25Q512A";
+                if (id[2] == 0x21) return "N25Q00AA";
+            }
+            if (id[1] == 0xBB) {
+                if (id[2] == 0x22) return "MT25QL02GC";
+            }
+        }
+        if (id[0] == 0xBF) {
+            // SST
+            if (id[1] == 0x25) {
+                if (id[2] == 0x02) return "SST25WF010";
+                if (id[2] == 0x03) return "SST25WF020";
+                if (id[2] == 0x04) return "SST25WF040";
+                if (id[2] == 0x41) return "SST25VF016B";
+                if (id[2] == 0x4A) return "SST25VF032";
+            }
+            if (id[1] == 0x25) {
+                if (id[2] == 0x01) return "SST26VF016";
+                if (id[2] == 0x02) return "SST26VF032";
+                if (id[2] == 0x43) return "SST26VF064";
+            }
+        }
+        return "(unknown chip)";
+    }
+
 public:
     void setup() {
         SPI.begin();
@@ -145,6 +180,48 @@ public:
         }
     }
 
+    void flashMemory() {
+        Serial.println("test: Checking flash memory...");
+
+        if (!SerialFlash.begin(PIN_FLASH_CS)) {
+            Serial.println("test: Flash memory FAILED");
+            return;
+        }
+
+        uint8_t buffer[256];
+
+        SerialFlash.readID(buffer);
+        if (buffer[0] == 0) {
+            Serial.println("test: Flash memory FAILED");
+            return;
+        }
+
+        uint32_t chipSize = SerialFlash.capacity(buffer);
+        if (chipSize == 0) {
+            Serial.println("test: Flash memory FAILED");
+            return;
+        }
+
+        Serial.println("Read Chip Identification:");
+        Serial.print("  JEDEC ID:     ");
+        Serial.print(buffer[0], HEX);
+        Serial.print(" ");
+        Serial.print(buffer[1], HEX);
+        Serial.print(" ");
+        Serial.println(buffer[2], HEX);
+        Serial.print("  Part Nummber: ");
+        Serial.println(id2chip(buffer));
+        Serial.print("  Memory Size:  ");
+        Serial.print(chipSize);
+        Serial.println(" bytes");
+        Serial.print("  Block Size:   ");
+        uint32_t blockSize = SerialFlash.blockSize();
+        Serial.print(blockSize);
+        Serial.println(" bytes");
+
+        Serial.println("test: Flash memory PASSED");
+    }
+
     void leds(bool on) {
         digitalWrite(13, on ? HIGH : LOW);
         digitalWrite(A3, on ? HIGH : LOW);
@@ -203,6 +280,7 @@ void setup() {
     while (true) {
         check.leds(false);
 
+        check.flashMemory();
         check.bno055();
         check.mpl3115a2();
         check.tsl2591();
